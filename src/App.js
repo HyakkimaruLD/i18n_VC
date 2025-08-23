e24a2f7b25e14602a5c4de351c46306ac22512c3
Vlad Chernukha <forreadtr@gmail.com>
2025-08-24 00:38:22 +0300

    Підготовка DrawerNavigation(вибір теми(темна, світла), вибір мови, вихід, профіль)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,87 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text } from 'react-native'
+import { StyleSheet, Text, View, Alert } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 import { useTranslation } from 'react-i18next'
 import { useEffect } from 'react'
-import { changeLanguage, loadLanguage } from './i18n'
+import { loadLanguage } from './i18n'
 
 import CalendarScreen from './Screens/CalendarScreen'
 import MapScreen from './Screens/MapScreen'
 import NewScreen from './Screens/NewScreen'
 
 const Tab = createBottomTabNavigator()
+const Drawer = createDrawerNavigator()
+
+//Tabs
+function Tabs() {
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ color }) => {
+                    let icon
+                    if (route.name === 'Calendar')
+                    {
+                        icon = '📅'
+                    }
+                    else if (route.name === 'Map')
+                    {
+                        icon = '🗺️'
+                    }
+                    else if (route.name === 'New')
+                    {
+                        icon = '➕'
+                    }
+                    return <Text style={{ fontSize: 18, color }}>{icon}</Text>
+                },
+                tabBarActiveTintColor: 'tomato',
+                tabBarInactiveTintColor: 'gray',
+            })}
+        >
+            <Tab.Screen name="Calendar" component={CalendarScreen} />
+            <Tab.Screen name="Map" component={MapScreen} />
+            <Tab.Screen name="New" component={NewScreen} />
+        </Tab.Navigator>
+    )
+}
+
+//Drawer screen
+function ThemeScreen() {
+    return (
+        <View style={styles.container}>
+            <Text>Вибір теми (поки не працює)</Text>
+        </View>
+    )
+}
+
+function LanguageScreen() {
+    return (
+        <View style={styles.container}>
+            <Text>Вибір мови (укр/англ, поки не працює)</Text>
+        </View>
+    )
+}
+
+function ProfileScreen() {
+    return (
+        <View style={styles.container}>
+            <Text>Це ви</Text>
+        </View>
+    )
+}
+
+function LogoutScreen({ navigation }) {
+    useEffect(() => {
+        Alert.alert('Ви вийшли')
+        navigation.goBack()
+    }, [])
+    return (
+        <View style={styles.container}>
+            <Text>Ви вийшли</Text>
+        </View>
+    )
+}
 
 export default function App() {
     const { t } = useTranslation()
@@ -21,32 +92,13 @@ export default function App() {
 
     return (
         <NavigationContainer>
-            <Tab.Navigator
-                screenOptions={({ route }) => ({
-                    tabBarIcon: ({ color }) => {
-                        let icon
-                        if (route.name === 'Calendar')
-                        {
-                            icon = '📅'
-                        }
-                        else if (route.name === 'Map')
-                        {
-                            icon = '🗺️'
-                        }
-                        else if (route.name === 'New')
-                        {
-                            icon = '➕'
-                        }
-                        return <Text style={{ fontSize: 18, color }}>{icon}</Text>
-                    },
-                    tabBarActiveTintColor: 'tomato',
-                    tabBarInactiveTintColor: 'gray',
-                })}
-            >
-                <Tab.Screen name="Calendar" component={CalendarScreen} />
-                <Tab.Screen name="Map" component={MapScreen} />
-                <Tab.Screen name="New" component={NewScreen} />
-            </Tab.Navigator>
+            <Drawer.Navigator initialRouteName="Home">
+                <Drawer.Screen name="Home" component={Tabs} />
+                <Drawer.Screen name="Theme" component={ThemeScreen} />
+                <Drawer.Screen name="Language" component={LanguageScreen} />
+                <Drawer.Screen name="Profile" component={ProfileScreen} />
+                <Drawer.Screen name="Logout" component={LogoutScreen} />
+            </Drawer.Navigator>
             <StatusBar style="auto" />
         </NavigationContainer>
     )
